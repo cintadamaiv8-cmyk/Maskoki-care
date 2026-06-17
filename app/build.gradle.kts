d6d5a70f34428d7afe336d6cd0e5a129fa5f48ba
cintadamaiv8-cmyk <cintadamaiv8@gmail.com>
2026-06-17 21:59:14 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,19 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.google.devtools.ksp)
+  alias(libs.plugins.roborazzi)
+  alias(libs.plugins.secrets)
+}
+
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+
+  compileSdk {
+    version = release(36) {
+      minorApiLevel = 1
+    }
+  }
 
   defaultConfig {
     applicationId = "com.aistudio.maskokicare.abzyx"
@@ -38,8 +51,7 @@ android {
     }
 
     debug {
-      // pakai debug keystore bawaan Android
-      // jangan pakai debugConfig custom
+      // pakai debug keystore otomatis
     }
   }
 
@@ -60,62 +72,81 @@ android {
   }
 }
 
+
 secrets {
   propertiesFileName = ".env"
   defaultPropertiesFileName = ".env.example"
 }
 
-// Some unused dependencies are commented out below instead of being removed.
-// This makes it easy to add them back in the future if needed.
+
+// dependencies
+
 dependencies {
+
   implementation(platform(libs.androidx.compose.bom))
   implementation(platform(libs.firebase.bom))
-  // implementation(libs.accompanist.permissions)
+
   implementation(libs.androidx.activity.compose)
-  // implementation(libs.androidx.camera.camera2)
-  // implementation(libs.androidx.camera.core)
-  // implementation(libs.androidx.camera.lifecycle)
-  // implementation(libs.androidx.camera.view)
+
   implementation(libs.androidx.compose.material.icons.core)
   implementation(libs.androidx.compose.material.icons.extended)
   implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.graphics)
   implementation(libs.androidx.compose.ui.tooling.preview)
+
   implementation(libs.androidx.core.ktx)
-  // implementation(libs.androidx.datastore.preferences)
+
   implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.lifecycle.viewmodel.compose)
+
   implementation(libs.androidx.navigation.compose)
+
   implementation(libs.androidx.room.ktx)
   implementation(libs.androidx.room.runtime)
+
   implementation(libs.coil.compose)
+
   implementation(libs.converter.moshi)
-  // implementation(libs.firebase.ai)
+
   implementation(libs.kotlinx.coroutines.android)
   implementation(libs.kotlinx.coroutines.core)
+
   implementation(libs.logging.interceptor)
+
   implementation(libs.moshi.kotlin)
+
   implementation(libs.okhttp)
-  // implementation(libs.play.services.location)
+
   implementation(libs.retrofit)
+
+
   testImplementation(libs.androidx.compose.ui.test.junit4)
   testImplementation(libs.androidx.core)
   testImplementation(libs.androidx.junit)
   testImplementation(libs.junit)
+
   testImplementation(libs.kotlinx.coroutines.test)
+
   testImplementation(libs.robolectric)
   testImplementation(libs.roborazzi)
   testImplementation(libs.roborazzi.compose)
   testImplementation(libs.roborazzi.junit.rule)
+
+
   androidTestImplementation(platform(libs.androidx.compose.bom))
+
   androidTestImplementation(libs.androidx.compose.ui.test.junit4)
   androidTestImplementation(libs.androidx.espresso.core)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.runner)
+
+
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
-  "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
+
+
+  ksp(libs.androidx.room.compiler)
+  ksp(libs.moshi.kotlin.codegen)
 }
